59db2b1f451a32975b40389b89663e5392a29bb2
DasunMuthuruwan <dasunmuthuruwan9@gmail.com>
2021-04-03 16:19:52 +0530

    Add votes, views and answers to question item

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -2,23 +2,30 @@
 @include('user.index')
 @section('content')
 <div class="col-md-12 justify-content-center">
-    <div class="card border border-success">
+    <div class="card border border-success mb-4">
         <div class="p-2 bg-info text-white">
             <h4 class="text-dark text-center">All Questions</h4>
         </div>
         <div class="card-body">
             @foreach ($questions as $question)
-                <div class="media mt-3">
-                    <div class="media-body">
-                        <div class="card border border-warning">
-                            <div class="card-body">
-                                <h5 class="text-info mt-0"><a href="{{ $question->url }}">{{ $question->new_title }}</a></h5>
-                                <p class="lead">
-                                    Asked By <a href="{{ $question->user->url }}" class="text-success"> {{$question->user->name}}</a>
-                                    <small class="text-muted" style="font-size: 16px;"> {{ $question->created_date }} </small>
-                                </p>
-                                {{ Str::limit($question->body, 250) }}
-                            </div>
+                <div class="card border border-warning">
+                    <div class="card-body">
+                        <h5 class="text-info mt-0"><a href="{{ $question->url }}">{{ $question->new_title }}</a></h5>
+                            <p class="lead">
+                                Asked By <a href="{{ $question->user->url }}" class="text-success"> {{$question->user->name}}</a>
+                                <small class="text-muted" style="font-size: 16px;"> {{ $question->created_date }} </small>
+                            </p>
+                            {{ Str::limit($question->body, 250) }}
+                    </div>
+                    <div class="d-flex flex-row counters justify-content-center mb-2">
+                        <div class="vote ml-4 pb-2">
+                            <strong>{{ $question->votes }}</strong> <em>{{ Str::plural('vote', $question->votes) }}</em>
+                        </div>
+                        <div class="status {{ $question->status }} ml-4 pb-2">
+                            <strong>{{ $question->answers }}</strong> <em>{{ Str::plural('answers', $question->answers) }}</em>
+                        </div>
+                        <div class="view ml-4 pb-2">
+                            <strong>{{ $question->views }}</strong> <em>{{Str::plural('views', $question->views) }}</em>
                         </div>
                     </div>
                 </div>
--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{config('app.name','User Management System')}}</title>
+        <title>{{config('app.name','Questions Answers Applications')}}</title>
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
